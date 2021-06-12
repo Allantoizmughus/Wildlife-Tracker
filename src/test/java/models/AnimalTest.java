eae63f16b432bd269486e51957e195c3dcb5e25e
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-13 02:47:41 +0300

    Add tests for get name and id in animal test file

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,6 +1,7 @@
 package models;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class AnimalTest extends TestCase {
 
@@ -10,4 +11,23 @@ public class AnimalTest extends TestCase {
 
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void returnAllInstancesOfAnimal_true(){
+        Animal animal=new Animal("Lion");
+        assertEquals(true, animal instanceof Animal);
+    }
+
+    @Test
+    public void returnNameOfAnimal_String(){
+        Animal animal=new Animal("Lion");
+        assertEquals("Lion", animal.getName());
+
+    }
+    @Test
+    public void returnIdOfAnimal_int(){
+        Animal animal=new Animal("Lion");
+        assertEquals(1,animal.getId());
+    }
+
 }
