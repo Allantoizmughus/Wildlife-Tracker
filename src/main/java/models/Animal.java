09e8f513ee5f08d3de3f9b99b97d29e3e2d1bcd4
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-13 02:33:12 +0300

    Create constructor for Animal class amd its getters

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Animal {
+    public String name;
+    public int id;
+
+    public Animal(String name){
+        this.name=name;
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
