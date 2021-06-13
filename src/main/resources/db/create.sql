0b16e146d5e5afb966601d01324c86929f0a8121
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-13 13:28:40 +0300

    Create table in sql

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,6 +1,14 @@
 SET MODE PostgreSQL;
 
-CREATE TABLE IF NOT EXISTS tasks (
+CREATE TABLE IF NOT EXISTS Animals (
+  id int PRIMARY KEY auto_increment,
+  name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS Endangered (
   id int PRIMARY KEY auto_increment,
   name VARCHAR,
+  endangered boolean,
+  health VARCHAR
+  age int
 );
