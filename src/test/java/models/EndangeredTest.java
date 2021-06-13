a3ad4dcd9435fb10138a76a3075fcdce5057b4a5
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-13 12:49:58 +0300

    Add tests 1 and 2 of Endangered class

--- src/test/java/models/EndangeredTest.java
+++ src/test/java/models/EndangeredTest.java
@@ -1,6 +1,7 @@
 package models;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class EndangeredTest extends TestCase {
 
@@ -10,4 +11,17 @@ public class EndangeredTest extends TestCase {
 
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void returnInstancesOfEndangered_true(){
+        Endangered newEndangered=new Endangered("white Rhino",1,true,"Okay",20);
+        assertEquals(true,newEndangered instanceof Endangered);
+    }
+
+    @Test
+    public void returnNameOfEndangered_String(){
+        Endangered newEndangered=new Endangered("white Rhino",1,true,"Okay",20);
+        assertEquals("Rhino",newEndangered.getName());
+    }
+
 }
